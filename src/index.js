15a375cce3f9a4c3fde3b0edde841410096444d2
Chad <cdhqwebdev@gmail.com>
2017-11-28 15:58:06 -0500

    add css styling, search bar functionality

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 const API_KEY = "AIzaSyDr7WIbSIu9GQ0mA6gTUSqb99N8VAimcLs";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: "mountain bikes" }, videos => {
-      this.setState({ videos });
+    this.videoSearch("Appalachian Trail");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
+      this.setState({ videos: videos, selectedVideo: videos[0] });
     });
   }
 
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
